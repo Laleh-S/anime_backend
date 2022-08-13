cc3c3b57661aa294eff4af9a1d57cf9c47025e1f
Laleh <laleh.shahidi78@gmail.com>
2022-08-13 12:13:28 +0100

    editted config environment

--- config/environment.py
+++ config/environment.py
@@ -1,5 +1,7 @@
 # postgresql: is our database driver which a protocol that a database uses.
 # localhost:5432: is postgress's port
-db_URI = "postgresql://postgres:laleh@localhost:5432/animes_db"
+# db_URI = "postgresql://postgres:laleh@localhost:5432/animes_db"
+# secret = "thisismyverysecretsecret"
 
-secret = "thisismyverysecretsecret"
+db_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:laleh@localhost:5432/animes_db')
+secret = os.getenv('SECRET', 'thisismyverysecretsecret')
